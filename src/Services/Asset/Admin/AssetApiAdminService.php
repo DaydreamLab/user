9e99b5554b729ca89707813566b30d2c5cffbb07
Jordan <jordan@daydream-lab.com>
2019-05-09 14:32:11 +0800

    fixed cms#91

--- src/Services/Asset/Admin/AssetApiAdminService.php
+++ src/Services/Asset/Admin/AssetApiAdminService.php
@@ -20,49 +20,4 @@ class AssetApiAdminService extends AssetApiService
         $this->assetApiMapAdminService = $assetApiMapAdminService;
         parent::__construct($repo);
     }
-
-
-    public function store(Collection $input)
-    {
-        if ($input->get('id') == null || $input->get('id') == '') {
-            $api = $this->add($input);
-            $this->assetApiMapAdminService->storeKeysMap(Helper::collect([
-                'asset_id'  => $input->asset_id,
-                'api_ids'   => [
-                    $api->id
-                ]
-            ]));
-
-            return $api;
-        }
-        else {
-            $map = $this->assetApiMapAdminService->findBy('api_id', '=', $input->id)->first();
-            $map->delete();
-            $this->assetApiMapAdminService->storeKeysMap(Helper::collect([
-                'asset_id'  => $input->asset_id,
-                'api_ids'   => [
-                    $input->id
-                ]
-            ]));
-            return $this->modify($input);
-        }
-    }
-
-
-    public function storeMaps(Collection $input)
-    {
-        foreach ($input->api_ids as $api_id) {
-            $api    = $this->find($api_id);
-            $update = $api->update(['asset_id' => $input->asset_id]);
-            if (!$update) {
-                $this->status = Str::upper(Str::snake($this->type.'StoreMapFail'));
-                $this->response = null;
-                return false;
-            }
-        }
-        $this->status = Str::upper(Str::snake($this->type.'StoreMapSuccess'));
-        $this->response = null;
-        return true;
-    }
-
 }
