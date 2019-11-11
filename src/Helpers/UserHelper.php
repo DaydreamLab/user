c3bdb6a5f60d83f2c6c0a4f5a27a99967d51cb61
Jordan <jordan@daydream-lab.com>
2019-11-11 11:27:01 +0800

    fix(bug): access token expired_at incorrect

--- src/Helpers/UserHelper.php
+++ src/Helpers/UserHelper.php
@@ -15,6 +15,7 @@ class UserHelper
         $tokenResult = $user->createToken(env('APP_NAME'));
         $token       = $tokenResult->token;
         $token->expires_at = now()->addSeconds(config('daydreamlab.user.token_expires_in'));
+        $token->save();
         $data['token']       = $tokenResult->accessToken;
         $data['first_name']  = $user->first_name;
         $data['last_name']   = $user->last_name;
