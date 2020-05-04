a51a2146d4892f85a6e4d3e97bfd134f42ac1a95
Jerry <Jerry@daydream-lab.com>
2020-05-04 10:52:24 +0800

    zerone user register

--- src/Requests/User/Front/UserFrontRegisterPost.php
+++ src/Requests/User/Front/UserFrontRegisterPost.php
@@ -29,7 +29,7 @@ class UserFrontRegisterPost extends AdminRequest
             'password'              => 'required|string|min:8|max:16',
             'password_confirmation' => 'required|same:password',
             'first_name'            => 'required|string',
-            'last_name'             => 'required|string',
+            'last_name'             => 'nullable|string',
             'gender'                => 'nullble|string',
             'image'                 => 'nullble|string',
             'phone'                 => 'nullble|string',
@@ -39,7 +39,18 @@ class UserFrontRegisterPost extends AdminRequest
             'city'                  => 'nullble|string',
             'district'              => 'nullble|string',
             'address'               => 'nullble|string',
-            'zipcode'               => 'nullble|string'
+            'zipcode'               => 'nullble|string',
+            'company_name'          => 'required|string',
+            'tax_id_number'         => 'nullable|string',
+            'company_tel_locale'    => 'nullable|string',
+            'company_tel_number'    => 'required|string',
+            'company_tel_extension' => 'nullable|string',
+            'mobile_phone'          => 'required|string',
+            'department'            => 'required|string',
+            'job_title'              => 'required|string',
+            'become_zerone_member'  => 'nullable|boolean',
+            'zerone_subscriptions'  => 'nullable|boolean',
+            'zerone_breaking_news'  => 'nullable|boolean'
         ];
     }
 }
