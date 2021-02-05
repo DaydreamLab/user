cc553ffb1494bbc7800cc6bfa2650c89c130a938
Jerry <Jerry@daydream-lab.com>
2021-02-05 17:48:02 +0800

    fix

--- src/Resources/User/Admin/Models/UserAdminResource.php
+++ src/Resources/User/Admin/Models/UserAdminResource.php
@@ -16,8 +16,8 @@ class UserAdminResource extends JsonResource
     {
         return [
             'email'                 => $this->email,
-            'firstName'             => $this->first_name,
-            'lastName'              => $this->last_name,
+            'first_name'             => $this->first_name,
+            'last_name'              => $this->last_name,
             'gender'                => $this->gendor,
             'image'                 => $this->image,
             'phone'                 => $this->phone,
