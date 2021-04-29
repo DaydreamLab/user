49c596fe0c7cdd2e97bf9ae28f66e878085567b7
Jerry <Jerry@daydream-lab.com>
2021-04-29 13:46:55 +0800

    修改使用者重設密碼

--- src/Services/User/UserService.php
+++ src/Services/User/UserService.php
@@ -3,6 +3,7 @@
 namespace DaydreamLab\User\Services\User;
 
 use DaydreamLab\User\Notifications\RegisteredNotification;
+use DaydreamLab\User\Notifications\ResetPasswordNotification;
 use DaydreamLab\User\Events\Add;
 use DaydreamLab\User\Events\Modify;
 use DaydreamLab\User\Events\Remove;
@@ -104,7 +105,8 @@ class UserService extends BaseService
                 'expired_at'    => Carbon::now()->addHours(3)
             ]));
             $this->status = 'NeedResetPassword';
-            $this->response = ['token' => $token->token];
+            $user->notify(new ResetPasswordNotification($user, $token));
+            $this->response = [];
             return;
         }
 
