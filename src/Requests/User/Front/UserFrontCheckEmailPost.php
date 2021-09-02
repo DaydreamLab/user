6ef6d7eb6014e7d037d9c3ef5357944fdea554ed
Jerry <Jerry@daydream-lab.com>
2021-09-02 16:19:52 +0800

    fix

--- src/Requests/User/Front/UserFrontCheckEmailPost.php
+++ src/Requests/User/Front/UserFrontCheckEmailPost.php
@@ -6,6 +6,7 @@ use DaydreamLab\JJAJ\Requests\AdminRequest;
 
 class UserFrontCheckEmailPost extends AdminRequest
 {
+    protected $needAuth = false;
     /**
      * Determine if the user is authorized to make this request.
      *
