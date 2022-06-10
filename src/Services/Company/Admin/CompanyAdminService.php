389110b709bc7fb8e58addf431b4767ea9423fa9
Jordan <ppuff1988@gmail.com>
2022-06-10 16:22:04 +0800

    fix: bug

--- src/Services/Company/Admin/CompanyAdminService.php
+++ src/Services/Company/Admin/CompanyAdminService.php
@@ -105,7 +105,7 @@ class CompanyAdminService extends CompanyService
     public function store(Collection $input)
     {
         $company = $this->findBy('vat', '=', $input->get('vat'))->first();
-        if ($company) {
+        if ($company && ($input->get('id') && $input->get('id') != $company->id)) {
             $this->status = 'VatExists';
             $this->response = [];
             return $this->response;
