edd7944eaf13844f67060bd2971e31e20138da6e
Jordan <jordan@daydream-lab.com>
2023-03-07 17:32:21 +0800

    fix: 修正公司資料匯出沒有過濾公司標記問題

--- src/Requests/Company/Admin/CompanyAdminExportPost.php
+++ src/Requests/Company/Admin/CompanyAdminExportPost.php
@@ -39,6 +39,7 @@ class CompanyAdminExportPost extends ListRequest
             'company_category' => 'nullable|integer',
             'company_industry' => 'nullable|integer',
             'haveMembers' => ['nullable', Rule::in([0, 1])],
+            'categoryNote'  => ['nullable', Rule::in(['無', '原廠', '競業', '員工', '黑名單'])]
         ];
 
         return array_merge(parent::rules(), $rules);
