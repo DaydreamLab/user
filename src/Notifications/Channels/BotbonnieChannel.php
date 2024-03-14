59877f271a4c3305795391497cb9927000500428
Jordan <jordan@daydream-lab.com>
2024-03-14 18:31:50 +0800

    fix: 調整line facebook 不進簡訊通知

--- src/Notifications/Channels/BotbonnieChannel.php
+++ src/Notifications/Channels/BotbonnieChannel.php
@@ -39,59 +39,25 @@ class BotbonnieChannel
         $toChannel = 'to' . Str::ucfirst(Str::lower($to['platform']));
         $message = $notification->$toChannel($notifiable);
         $messageContent = strip_tags($message->content);
-        $messageLength = mb_strlen($messageContent, 'UTF-8');
 
         $post = $this->getPostData($to, $messageContent);
 
-        if (config('daydreamlab.user.sms.env') == 'local') {
-            $sendResult = true;
-            $msgId = '';
-            $response = [];
-        } else {
+        try {
             $response = (new Client())->post($this->baseUrl, $post);
-            try {
-                $response_content = json_decode($response->getBody()->getContents());
-            } catch (\Throwable $t) {
-                SmsDebug::create([
-                    'payload' => $post['json'],
-                    'response' => $response->getBody(),
-                    'historyId' => null
-                ]);
-            }
-            $statusCode = $response->getStatusCode();
-            $sendResult = $response_content->res;
-            $msgId = '';
-        }
-
-
-        if (config('daydreamlab.user.sms.log')) {
-            $data = [
-                'notificationId' => $notification->notificationId ?? null,
-                'phoneCode'     => '',
-                'phone'         => '',
-                'category'      => $message->category,
-                'type'          => $message->type,
-                'messageId'     => $msgId,
-                'message'       => $message->content,
-                'messageLength' => $messageLength,
-                'messageCount'  => 0,
-                'success'       => $sendResult ? 1: 0,
-                'responseCode'  => $statusCode ?? '',
-                'created_by'    => $message->creatorId
-            ];
-
-            $data = array_merge($data, $message->extraFields);
-            $history = SmsHistory::create($data);
-        }
-
-        if (config('daydreamlab.user.sms.debug')) {
+        } catch (\Throwable $t) {
             SmsDebug::create([
                 'payload' => $post['json'],
-                'response' => $sendResult,
-                'historyId' => isset($history) ? $history->id : null
+                'response' => $t->getMessage(),
+                'historyId' => null
             ]);
         }
 
-        return $sendResult;
+        if ($response->getStatusCode() !== 200) {
+            SmsDebug::create([
+                'payload' => $post['json'],
+                'response' => json_decode($response->getBody()->getContents()),
+                'historyId' => null
+            ]);
+        }
     }
 }
